1e03e7b77bd21d44c1ef8a27c98f7cee873b5bc8
AlexMureti <alexmkmaingi@gmail.com>
2025-09-19 16:45:18 +0300

    task-lister-mini-project

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById("new-task-description").value;
+
+    const li = document.createElement("li");
+    li.textContent = taskDescription;
+    taskList.appendChild(li);
+
+    form.reset();
+  });
+});
